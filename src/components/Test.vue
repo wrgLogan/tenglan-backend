1c5a45d0c4fe8d0936c7c8fa9a98ea6a2c997b1b
LOGAN-THINKPAD\wrglo <wrglogan@foxmail.com>
2017-11-26 23:52:58 +0800

    init

---
+++ src/components/Test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page">
+    <h1>test</h1>
+    <el-button  type="primary" icon="el-icon-search" v-on:click="switchTo">切换</el-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    switchTo: function () {
+      this.$switchTo('/', 'forward')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
